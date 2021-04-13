b6adefada930fd4b2581d13976c5b548e7d5ded7
Marc-André Appel <marco@hybride.dev>
2021-04-13 14:51:42 +0200

    feat: creates the project with a basic UUID trait

---
+++ src/Uuids.php
@@ -0,0 +1,35 @@
+<?php
+declare(strict_types=1);
+
+namespace MarcAndreAppel\LaravelUuids;
+
+use Illuminate\Support\Str;
+
+trait Uuids
+{
+    protected static function bootUuids()
+    {
+        static::creating(function ($model) {
+            $model->keyType = 'string';
+            $model->incrementing = false;
+            $model->casts = array_merge($model->casts, ['id' => 'string']);
+
+            $model->{$model->getKeyName()} = $model->{$model->getKeyName()} ?: Str::orderedUuid()->toString();
+        });
+    }
+
+    public function getIncrementing()
+    {
+        return false;
+    }
+
+    public function getKeyType()
+    {
+        return 'string';
+    }
+
+    public function getRouteKey(): string
+    {
+        return 'uuid';
+    }
+}
